a7f1a850cca46a5f006fa4a8dcce9cba67c057cf
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2020-05-25 17:44:07 -0300

    Alterações na página de Membros
    Alterações iniciais na página de Membros, baseado na imagem da Interface, além de outras alterações gerais na Navbar e removido o Footer

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -4,8 +4,7 @@ import Main from '../template/Main'
 export default props =>
     <Main icon="home" title="Início"
         subtitle="Segundo Projeto do capítulo de React.">
-        <div className='display-4'>Bem Vindo!</div>
+        <div className='display-4'>Bem-vindo Administrador!</div>
         <hr />
-        <p className="mb-0">Sistema para exemplificar a construção
-            de um cadastro desenvolvido em React!</p>
+        <p className="mb-0">Sistema para Gerenciamento de Grupos</p>
     </Main>
