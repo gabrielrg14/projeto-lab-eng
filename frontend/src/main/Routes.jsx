478aa6d6410b53ea79f2f57bb1eabaa99555cf5b
Goeiji <53585549+Goeiji@users.noreply.github.com>
2020-06-07 10:47:12 -0300

    Componente_Musica
    Adição do componente música

--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -4,11 +4,13 @@ import { Switch, Route, Redirect } from 'react-router'
 import Home from '../components/home/Home'
 import Membros from '../components/membros/Membros'
 import Materiais from '../components/materiais/Materiais'
+import Musica from '../components/musica/Musicas'
 
 export default props => 
     <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/membros' component={Membros} />
         <Route path='/materiais' component={Materiais} />
+        <Route path='/musicas' component={Musica} />
         <Redirect from='*' to='/' />
     </Switch>
